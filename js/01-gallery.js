72cd36d38cb913d2700f96a0e8e7c0baf5245aea
Fedir97 <106695191+Fedir97@users.noreply.github.com>
2022-10-09 22:33:54 +0300

    Add files via upload

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,43 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 const mainGallery = document.querySelector(".gallery");
-const jsBox = document.querySelector(".js-lightbox");
-const imageRef = document.querySelector('.lightbox__image');
-const closeBtn = document.querySelector('[data-action="close-lightbox"]');
 
-const createGallery = ({preview,original,description}) =>
- `<div class="gallery__item">
- <a class="gallery__link" href=${original}>
-   <img
-     class="gallery__image"
-     src=${preview}
-     data-source=${original}
-     alt=${description}
-   />
- </a>
-</div>`
-
-
-const galleryMarkup = galleryItems.reduce((acc, img) => acc + createGallery(img),"");
-mainGallery.insertAdjacentHTML("afterbegin", galleryMarkup);
-mainGallery.addEventListener("click", onPictureClick);
-closeBtn.addEventListener("click", onCloseBtnClick);
-
-function onPictureClick(event) {
-  event.preventDefault();
-  if (!event.target.classList.contains('gallery__image')) {
-    return;
-  }
-  
-  jsBox.classList.add('is-open');
-  imageRef.src = event.target.getAttribute("data-source")
-    imageRef.alt = event.target.alt
-    imageRef.description = event.target.decription
+function createGallary(items) {
+  return items
+    .map(
+      (item) => `<div class="gallery__item">
+                  <a class="gallery__link" href="${item.original}">
+                    <img
+                      class="gallery__image"
+                      src="${item.preview}"
+                      data-source="${item.original}"
+                      alt="${item.description}"
+                    />
+                  </a>
+                </div>`
+    )
+    .join("");
 }
 
-function onCloseBtnClick() {
-  jsBox.classList.remove('is-open');
-  imageRef.src = "";
-    imageRef.alt = "";
-    imageRef.description = "";}
+const addGallary = createGallary(galleryItems);
+
+mainGallery.innerHTML = addGallary;
+
+mainGallery.addEventListener("click", clickOnImage);
+
+function clickOnImage(e) {
+  e.preventDefault()
+
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const module = basicLightbox.create(`
+      <img src="${e.target.dataset.source}" width="800" height="600">
+  `);
+  module.show();
+
+}
+
+
